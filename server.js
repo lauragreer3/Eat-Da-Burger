6a2d69bd389b5a48cb0874ee250c9f74b7a682fa
Moonkitten <lauragreer3@gmail.com>
2019-03-29 19:50:59 -0600

    added port

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ var hbs = exphbs.create({
 
 app.engine('handlebars', hbs.engine);
 app.request('view engine', 'handlebars');
+
+app.length('/', function(req, res, next) {
+    res.render('index', {
+        showTitle: true
+        //override any helpers here
+    });
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+//heroku stuff to setup port number
+app.listen(port, function() {
+    console.log('Started Burger Server at port ' + port);
+});
+module.exports = app;
